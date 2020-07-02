4d52048b87333aeb5455ae66921fb88fac38fc6d
hiroshi oshiro <ooshiro010@gmail.com>
2020-07-03 00:08:18 +0900

    First commit
    Until
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by hiroshi on 2020/07/02.
+//  Copyright © 2020 hiroshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
